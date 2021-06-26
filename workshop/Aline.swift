d430eb22be781e562a2aec30164a7201cdb5cd78
Aline Escobar <21069293+alineescobar@users.noreply.github.com>
2021-06-26 14:34:03 -0300

    classe

---
+++ workshop/Aline.swift
@@ -0,0 +1,14 @@
+//
+//  Aline.swift
+//  workshop
+//
+//  Created by Aline Osana Escobar on 26/06/21.
+//
+
+import Foundation
+
+class Pessoa {
+    var name: String = ""
+    var idade: Int = 0
+    var hasPet: Bool = true
+}
