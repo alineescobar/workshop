473274c23f2d1f3e8adf6d563b110cfa86dff2d2
Rodrigo Kroef Tarouco <rodrigoktarouco@gmail.com>
2021-06-26 14:32:04 -0300

    First commit

---
+++ RodrigoClassTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RodrigoClassTableViewController.swift
+//  workshop
+//
+//  Created by Rodrigo Kroef Tarouco on 26/06/21.
+//
+
+import UIKit
+
+class RodrigoClassTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+}
