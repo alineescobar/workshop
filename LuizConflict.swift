a52d9062a415429381337b24049c31baf81e026e
Luiz Eduardo <luizdudumr@gmail.com>
2021-06-26 14:31:36 -0300

    commit luiz

---
+++ LuizConflict.swift
@@ -0,0 +1,20 @@
+//
+//  LuizConflict.swift
+//  workshop
+//
+//  Created by Luiz Eduardo Mello dos Reis on 26/06/21.
+//
+
+import SwiftUI
+
+struct LuizConflict: View {
+    var body: some View {
+        Text("oi zoom")
+    }
+}
+
+struct LuizConflict_Previews: PreviewProvider {
+    static var previews: some View {
+        LuizConflict()
+    }
+}
