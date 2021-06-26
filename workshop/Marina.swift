fe88f04fe78cd5ade1b6e935417b52bb9e7e8546
Marina De Pazzi <pazzi.dev@gmail.com>
2021-06-26 14:35:18 -0300

    chore: adicionando meu arquivo

---
+++ workshop/Marina.swift
@@ -0,0 +1,20 @@
+//
+//  Marina.swift
+//  workshop
+//
+//  Created by Marina De Pazzi on 26/06/21.
+//
+
+import SwiftUI
+
+struct Marina: View {
+    var body: some View {
+        Text("Minha alteração")
+    }
+}
+
+struct Marina_Previews: PreviewProvider {
+    static var previews: some View {
+        Marina()
+    }
+}
